27764fa0e219cc058187dc88450a87935aec2fd1
TMaropela <t.maropela@gmail.com>
2024-03-24 14:24:30 -0700

    Adding a script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+A script that starts a Flask web application
+listening on 0.0.0.0, port 5000
 """
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function that displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Function that displays HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """Function that displays C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=None)
