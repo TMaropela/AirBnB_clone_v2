ece4f6e104f3627fd76a3d21a28b9d761462b4be
TMaropela <t.maropela@gmail.com>
2024-03-24 14:29:12 -0700

    Adding a script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,17 +34,12 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontext(text="is cool"):
+    """Function that displays Python followed by value of text variable"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=None)
