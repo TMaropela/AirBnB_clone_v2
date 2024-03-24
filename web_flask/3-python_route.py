699001b65ab2ad8e19ad428e11c3cee2670740bd
TMaropela <t.maropela@gmail.com>
2024-03-24 14:32:28 -0700

    Adding a script that starts a Flask web application

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
-"""Startinh a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
+"""
+A script that starts a Flask web application
+listening on 0.0.0.0, port 5000
 """
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function that displays Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Function that displays HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>.
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """Function that displays C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
